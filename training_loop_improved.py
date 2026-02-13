10fe7e635fecbfb3765c5361f09047d75f760c41
Ryalith <quelin.lucie@gmail.com>
2026-02-13 11:51:34 +0100

    Improved training loop, fixed typos in datasets

--- training_loop_improved.py
+++ training_loop_improved.py
@@ -5,7 +5,7 @@ from datasets import concatenate_datasets
 from torchvision import transforms as T
 
 from oadino.clevr import load_clevertex_dataset, load_clevr_dataset
-from oadino.models import ConvVAE64, OADinoModel, OADinoPreProcessor
+from oadino.models import ConvVAE16, ConvVAE64, OADinoModel, OADinoPreProcessor
 from oadino.training import Trainer
 
 # TODO: create merged dataset with images from both CLEVR and CLEVRTEX with 4K images
@@ -18,9 +18,10 @@ img_size = 224
 
 transform = T.Compose(
     [
-        T.ToTensor(),
+        T.Lambda(lambda img: img.convert("RGB")),  # Convert RGBA/grayscale to RGB
         T.Resize(img_size + int(img_size * 0.01) * 10),
         T.CenterCrop(img_size),
+        T.ToTensor(),
         T.Normalize([0.5], [0.5]),
     ]
 )
@@ -30,9 +31,9 @@ clevr_train_dataset, clevr_test_dataset, _ = load_clevr_dataset(
     clevr_path, 2048, transform
 )
 
-clevrtex = Path("/ssd2/mldata/CLEVRTexV2/clevrtexv2_full/")
+clevrtex_path = Path("/ssd2/mldata/CLEVRTexV2/clevrtexv2_full/")
 clevrtex_train_dataset, clevrtex_test_dataset, _ = load_clevertex_dataset(
-    clevr_path, 2048, transform
+    clevrtex_path, 2048, transform
 )
 
 # Merge train datasets
@@ -41,28 +42,42 @@ combined_train = concatenate_datasets([clevr_train_dataset, clevrtex_train_datas
 # Merge test datasets
 combined_test = concatenate_datasets([clevr_test_dataset, clevrtex_test_dataset])
 
+
+def apply_transform(batch):
+    batch["image"] = [transform(img) for img in batch["image"]]
+    return batch
+
+
+combined_train.set_transform(apply_transform)
+combined_test.set_transform(apply_transform)
+
 train_dataset_name = "CLEVR_CLEVRTex_train_4K_224"
 
 ### INSTANCIATE MODELS
 img_size = 224
-dinov2_vitb14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14")
-dinov2_vitb14 = dinov2_vitb14.cuda()
-dinov2_vitb14.eval()
+# dinov2_vitb14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14")
+# dinov2_vitb14 = dinov2_vitb14.cuda()
+# dinov2_vitb14.eval()
+dinov2_vits14 = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
+dinov2_vits14 = dinov2_vits14.cuda()
+dinov2_vits14.eval()
 
 
 model = OADinoModel(ConvVAE64())
-pre_processor = OADinoPreProcessor(dinov2_vitb14)
+# model = OADinoModel(ConvVAE64())
+# pre_processor = OADinoPreProcessor(dinov2_vitb14)
+pre_processor = OADinoPreProcessor(dinov2_vits14)
 
 ### Train Models
 
 trainer = Trainer(
     pre_processor,
     model,
-    "dinov2_vitb14",
+    # "dinov2_vitb14",
+    "dinov2_vits14",
     combined_train,
     train_dataset_name,
     combined_test,
     224,
 )
-trainer.train("/ssd2/mldata/oadino", 15)
-
+trainer.train("/ssd2/mldata/oadino", 20)
